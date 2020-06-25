384421fabe867b3ccaed132a7c3351a60aefb865
khossei4 <kamyarh65@gmail.com>
2020-06-25 14:17:49 -0400

    Style Update

--- digital-dash/src/App.tsx
+++ digital-dash/src/App.tsx
@@ -1,83 +1,123 @@
-import React from 'react';
-import { Tabs, AppBar, Tab, makeStyles, ThemeProvider, createMuiTheme, Grid, Typography } from '@material-ui/core';
-import './App.css';
+import React from "react";
+import {
+  Tabs,
+  AppBar,
+  Tab,
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import "./App.css";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect,
 } from "react-router-dom";
-import { HomePage } from './components/HomePage/HomePage';
-import defaultTheme from './defaultTheme';
-import logo from './assets/logo.jpg';
+import { HomePage } from "./components/HomePage/HomePage";
+import defaultTheme from "./defaultTheme";
+import logo from "./assets/logo.jpg";
 
 const useStyles = makeStyles({
   navBar: {
-    justifyContent: 'flex-end'
+    display: "flex",
+    alignItems: "center",
+    height:"108px"
+  },
+  navLogo: {
+
   },
   navTab: {
-    fontSize: '0.5rem'
+    fontSize: "0.5rem",
+    
   },
   logo: {
-    height: '3rem',
-    width: 'auto',
+    height: "3rem",
+    width: "auto",
   },
   topHeading: {
-    padding: '1rem 2rem',
+    padding: "1rem 2rem",
   },
   topHeadingText: {
-    display: 'flex',
-    alignItems: 'center',
-    letterSpacing: '1em',
+    paddingLeft: "5px",
+    display: "flex",
+    alignItems: "center",
+  },
+  body:{
+    height: "1080px"
   }
-
-})
-
+});
 
 function App() {
   const theme = createMuiTheme(defaultTheme);
   const classes = useStyles();
-  const [navTabValue, setNavTabValue] = React.useState('home');
+  const [navTabValue, setNavTabValue] = React.useState("home");
   const handleNavChange = (event: any, tab: any) => {
     setNavTabValue(tab);
-  }
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Grid container direction='row' justify='space-between' className={classes.topHeading} >
-          <Typography variant='h1' align='left' className={classes.topHeadingText} >DigitalDash</Typography>
-          <img className={classes.logo} src={logo} />
+        <Grid container direction="row" className={classes.body}>
+          <Grid container md={6} className={classes.navBar}>
+            <img className={classes.logo} src={logo} />
+            <Typography align="left" className={classes.topHeadingText}>
+              DigitalDash
+            </Typography>
+          </Grid>
+
+          <Router>
+            <Grid container justify="center" md={6} style={{paddingTop:"20px",paddingBottom:"35px"}}>
+              <Tabs
+                value={navTabValue}
+                onChange={handleNavChange}
+                className={classes.navTab}
+
+              >
+                <Tab
+                  className={classes.navTab}
+                  value="home"
+                  label="home"
+                  component={Link}
+                  to={{ pathname: "/home" }}
+                />
+                <Tab
+                  className={classes.navTab}
+                  value="projects"
+                  label="projects"
+                  component={Link}
+                  to={{ pathname: "/projects" }}
+                />
+                <Tab
+                  className={classes.navTab}
+                  value="careers"
+                  label="careers"
+                  component={Link}
+                  to={{ pathname: "/careers" }}
+                />
+                <Tab
+                  className={classes.navTab}
+                  value="contact"
+                  label="contact"
+                  component={Link}
+                  to={{ pathname: "/contact" }}
+                />
+              </Tabs>
+            </Grid>
+            <Grid md={12} className={classes.body}>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+              <Route path="/home" component={HomePage} />
+            </Switch>
+            </Grid>
+          </Router>
         </Grid>
-        <Router>
-          <Tabs
-            value={navTabValue}
-            onChange={handleNavChange}
-            classes={{
-              flexContainer: classes.navBar,
-            }}
-          >
-            <Tab
-              className={classes.navTab}
-              value='home'
-              label='home'
-              component={Link}
-              to={{ pathname: '/home' }}
-            />
-            <Tab
-              className={classes.navTab}
-              value='projects'
-              label='projects'
-              component={Link}
-              to={{ pathname: '/projects' }}
-            />
-
-          </Tabs>
-          <Switch>
-            <Route path='/home' component={HomePage} />
-          </Switch>
-        </Router>
-
       </div>
     </ThemeProvider>
   );
--- digital-dash/src/components/HomePage/HomePage.tsx
+++ digital-dash/src/components/HomePage/HomePage.tsx
@@ -1,89 +1,322 @@
-import { makeStyles, Container, Grid, AppBar, Typography, Tabs, Tab, Button } from '@material-ui/core';
-import { TabPanel } from '@material-ui/lab';
-import React, { ReactElement } from 'react';
+import {
+  makeStyles,
+  Container,
+  Grid,
+  AppBar,
+  Typography,
+  Tabs,
+  Tab,
+  Button,
+} from "@material-ui/core";
+import { TabPanel } from "@material-ui/lab";
+import React, { ReactElement } from "react";
 // import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image, DotGroup } from 'pure-react-carousel';
-import Carousel from 'react-material-ui-carousel'
-import  featuredA  from '../../assets/picA.jpg';
-import featuredB from '../../assets/picB.jpg';
-import featuredC from '../../assets/picC.jpg';
-import featuredD from '../../assets/picD.jpg';
-import picture1 from '../../assets/pic1.jpg';
 
+import featuredA from "../../assets/svg/BuildBetterProducts_ArtWork.svg";
+import featuredB from "../../assets/picB.jpg";
+import featuredC from "../../assets/picC.jpg";
+import featuredD from "../../assets/picD.jpg";
+
+import icon from "../../assets/svg/Icon-arrow-down.svg";
+import externalIcon from "../../assets/svg/Icon-external-link.svg";
+import WhoWeAre from "../../assets/svg/WhoWeAre_ArtWork.svg";
+import WhatWeDo from "../../assets/svg/WhatWeDo_ArtWork.svg";
+import ProjectR from "../../assets/svg/ProjectR_Artwork.svg";
+import Careers from "../../assets/svg/Careers_ArtWork.svg";
 
 const useStyles = makeStyles((theme) => ({
-    baseContainer: {
-        height: '100%',
-    },
-    carouselContainer: {
-        width:'100%',
-        margin: '2rem 0',
-    },
-    carouselButton: {
-        textDecoration: 'none',
-        backgroundColor: 'rgba(0,0,0,0)',
-        padding: '0',
-        borderColor: 'rgba(0,0,0,0)',
-    },
-    carouselSlide: {
-    },
-    carouselImage: {
-        width: '100%',
-        height: '30rem',
-        objectFit: 'cover',
-    },
-    ourStoryImage: {
-        maxWidth:'100%',
-        objectFit:'cover',
-        borderRadius: '2px',
-    },
-    ourStoryHeading: {
-        borderBottomColor: '#78909c',
-        borderBottomStyle: 'solid',
-        borderBottomWidth: '3px',
-    },
-    ourStoryBodyContainer: {
-        marginTop: '1rem',
-    }
+  baseContainer: {
+    height: "100%",
+  },
+  Headings: {
+    fontFamily: "Karla",
+    fontSize: "50px",
+    width: "316px",
+  },
+  subHeadings: {
+    fontFamily: "Karla",
+    fontSize: "30px",
+
+    color: "#7C7C7C",
+  },
+  carouselContainer: {
+    width: "100%",
+    margin: "2rem 0",
+  },
+
+  carouselSlide: {},
+  carouselImage: {
+    width: "976px",
+    height: "536px",
+    objectFit: "cover",
+  },
+  BuildProducts: {
+    width: "1453px",
+    margin: "auto",
+  },
+  Home: {
+    margin: "auto",
+  },
+  icons: {
+    height: "20px",
+    width: "20px",
+  },
+  WhoWeAre: {
+    marginTop: "52px",
+  },
+  WhoWeAreTxt: {
+    width: "620px",
+  },
+  projHeadings: {
+    fontFamily: "Karla",
+    fontSize: "15px",
+    color: "#7C7C7C",
+  },
 }));
 
 export const HomePage = (): ReactElement => {
-    const classes = useStyles();
-    const [imgTabValue, setImgTabValue] = React.useState(0)
-    const carouselImages = [featuredA,featuredB,featuredC,featuredD]
-    const handleImgTabChange = (event: React.ChangeEvent<{}>, tab: any): void => {
-        setImgTabValue(tab)
-    }
-    return (
-        <Container disableGutters className={classes.baseContainer}>
-            <Grid container direction='column' justify='center'>
-                
-                <Grid container direction='row' justify='center' >
-                <Carousel className={classes.carouselContainer}>
-                    {carouselImages.map((img, idx) => {
-                        return (
-                            <Grid item>
-                                <img className={classes.carouselImage} src={img}/>
-                            </Grid>
-                        )
-                    })
-                    }
-                </Carousel>
-                </Grid>
-                <Typography variant='h3' align='left' className={classes.ourStoryHeading}>Our Story</Typography>
-                <Grid container direction='row' justify='center' className={classes.ourStoryBodyContainer}>
-                    <Grid item direction='column' sm={12} md={6}>
-                        <Typography variant='body1'>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </Typography>
-                    </Grid>
-                    <Grid item direction='column' sm={12} md={6}>
-                        <img src={ picture1 } className={classes.ourStoryImage}/>
-                    </Grid>
-                </Grid>
+  const classes = useStyles();
+  const [navTabValue, setNavTabValue] = React.useState("ResumeBuilder");
+  const handleNavChange = (event: any, tab: any) => {
+    setNavTabValue(tab);
+  };
+  return (
+    <Container disableGutters className={classes.baseContainer} maxWidth="xl">
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        className={classes.Home}
+        style={{ marginTop: "120px" }}
+      >
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          className={classes.BuildProducts}
+        >
+          <Grid item direction="column">
+            <Typography variant="body2" className={classes.Headings}>
+              Build Better Products
+            </Typography>
+            <Typography
+              variant="body2"
+              className={classes.subHeadings}
+              style={{ width: "316px", paddingTop: "18px" }}
+            >
+              Lorem ipsum dolor sit amet, consectetur dolor sit Lorem ipsum
+            </Typography>
+            <img
+              className={classes.icons}
+              src={icon}
+              style={{ marginTop: "22px" }}
+            />
+          </Grid>
+          <Grid item>
+            <img className={classes.carouselImage} src={featuredA} />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container className={classes.Home} style={{ marginTop: "181px" }}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          className={classes.BuildProducts}
+        >
+          <Grid container item direction="row">
+            <Grid item>
+              <Typography variant="body2" className={classes.Headings}>
+                Who We Are
+              </Typography>
+              <img
+                src={WhoWeAre}
+                className={classes.WhoWeAre}
+                style={{ marginRight: "52px" }}
+              />
             </Grid>
+            <Grid item>
+              <Typography
+                variant="body2"
+                className={classes.subHeadings}
+                style={{
+                  width: "620px",
+                  marginTop: "100px",
+                  paddingTop: "18px",
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur dolor sit Lorem ipsum
+                lorem ipsum dolor sit amet, consectetur dolor lorem ipsum dolor
+                sit amet, consectetur dolor lorem ipsum dolor sit amet,
+                consectetur dolor
+              </Typography>
+            </Grid>
+          </Grid>
 
-        </Container>
-    )
+          <Grid container item direction="row" justify="flex-end">
+            <Grid item>
+              <Typography
+                variant="body2"
+                className={classes.subHeadings}
+                style={{
+                  width: "620px",
+                  marginTop: "100px",
+                  paddingTop: "18px",
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur dolor sit Lorem ipsum
+                lorem ipsum dolor sit amet, consectetur dolor lorem ipsum dolor
+                sit amet, consectetur dolor lorem ipsum dolor sit amet,
+                consectetur dolor
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                variant="body2"
+                className={classes.Headings}
+                style={{ marginLeft: "100px" }}
+              >
+                What We Do
+              </Typography>
+              <img
+                src={WhatWeDo}
+                className={classes.WhoWeAre}
+                style={{ marginLeft: "52px" }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        className={classes.Home}
+        style={{ marginTop: "250px" }}
+      >
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          className={classes.BuildProducts}
+        >
+          <Grid item direction="column">
+            <Typography variant="body2" className={classes.subHeadings}>
+              Some of our work
+            </Typography>
+            <Typography variant="body2" className={classes.Headings}>
+              Projects
+            </Typography>
+            <Tabs value={navTabValue} onChange={handleNavChange}>
+              <Tab
+                value="ResumeBuilder"
+                label="Resume Builder"
+                className={classes.projHeadings}
+              />
+              <Tab
+                value="MyRef"
+                label="MyRef"
+                className={classes.projHeadings}
+              />
+              <Tab
+                value="JobMatch"
+                label="Job Match"
+                className={classes.projHeadings}
+              />
+            </Tabs>
+            <Typography
+              variant="body2"
+              style={{
+                fontFamily: "karla",
+                fontSize: "70px",
+                paddingTop: "30px",
+              }}
+            >
+              Resume Builder
+            </Typography>
+            <Typography
+              variant="body2"
+              className={classes.subHeadings}
+              style={{
+                width: "614px",
+                paddingTop: "50px",
+                paddingBottom: "45px",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur dolor sit Lorem ipsum
+              lorem ipsum dolor sit amet, consectetur dolor lorem ipsum dolor
+              sit amet, consectetur dolor lorem ipsum dolor sit amet,
+              consectetur dolor
+            </Typography>
+            <Grid
+              container
+              item
+              className={classes.subHeadings}
+              style={{ color: "#5F90FF" }}
+            >
+              <Grid item>
+                <Typography variant="body2">Learn More</Typography>
+              </Grid>
+              <img
+                src={externalIcon}
+                style={{ width: "27px", height: "27px", paddingLeft: "24px" }}
+              />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <img
+              src={ProjectR}
+              style={{ paddingLeft: "150px", paddingTop: "40px" }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
 
-}
-
+      <Grid
+        container
+        direction="column"
+        style={{ marginTop: "250px", background: "#EFEFEF" }}
+      >
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          className={classes.BuildProducts}
+        >
+          <Grid item direction="column">
+            <Typography
+              variant="body2"
+              className={classes.Headings}
+              style={{ paddingTop: "50px", fontWeight: "bold" }}
+            >
+              Careers
+            </Typography>
+          </Grid>
+          <Grid
+            container
+            item
+            justify="center"
+            style={{ paddingTop: "240px", paddingLeft: "50px" }}
+          >
+            <Grid item>
+              <Typography
+                className={classes.Headings}
+                style={{ fontWeight: "bold", width: "720px",marginBottom:"40px" }}
+              >
+                We’re looking for people who want to make a difference.
+              </Typography>
+              <Button variant="contained" className={classes.subHeadings} style={{background:"#6C63FF",color:"white",fontWeight:"bold", borderRadius:"30px", paddingLeft:"64px",paddingRight:"64px"}}>
+                View Opportunities
+              </Button>
+            </Grid>
+            <Grid item>
+              <img src={Careers} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+    
